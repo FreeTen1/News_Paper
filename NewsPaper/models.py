87db0f71168e09b4cf3af5cd5c3785de7f15febe
Misha <mishatambov@gmail.com>
2021-02-28 15:36:16 +0300

    работа с views
    Добавил html страницу для вывода всех постов, изменения в файлах

--- NewsPaper/models.py
+++ NewsPaper/models.py
@@ -53,6 +53,9 @@ class Post(models.Model):
     def preview(self):
         return self.post_text[:124] + '...'
 
+    def __str__(self):
+        return f'{self.type} : {self.post_heading}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
