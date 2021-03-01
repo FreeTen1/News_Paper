5e6df01977aee153fa13b22a4743dc623281151f
Misha <mishatambov@gmail.com>
2021-03-01 17:27:02 +0300

    Вывод новостей и одной новости
    Разобрался как выводить только новости, отредактировал шаблоны

--- NewsPaper/views.py
+++ NewsPaper/views.py
@@ -8,9 +8,11 @@ class NewsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
+    queryset = Post.objects.filter(type='NE').order_by('-post_date_creation')
+
 
 class NewsDetail(DetailView):
     model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
     template_name = 'single_news.html'  # название шаблона будет product.html
     context_object_name = 'single_news'  # название объекта. в нём будет
-
+    queryset = Post.objects.filter(type='NE')
