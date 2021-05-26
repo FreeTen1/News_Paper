20200061005ca8de300207d37fb7323d0d935878
Misha <mishatambov@gmail.com>
2021-05-26 22:30:26 +0300

    Revert "создал модель, затем удалил её, теперь не могу создать заново))))"
    This reverts commit f0ddcebac1647baf8f4c660e3aaaa244d17d2b6d.

--- NewsPaper/admin.py
+++ NewsPaper/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from NewsPaper.models import Author, Category, Post, PostCategory, Comment, Subscribers
+from NewsPaper.models import Author, Category, Post, PostCategory, Comment
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
-admin.site.register(Subscribers)
--- NewsPaper/models.py
+++ NewsPaper/models.py
@@ -62,24 +62,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/news/{self.id}'
 
-    def __str__(self):
-        return f'{self.id} {self.post_heading}'
-
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.post}'
-
-
-class Subscribers(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user} {self.category}'
 
 
 class Comment(models.Model):
